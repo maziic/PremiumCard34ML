00b77b8026ef97687efbda53655b753d4a9504cf
youssefEmadbd <youssef.emad1@yahoo.com>
2023-11-15 19:44:56 +0200

    Initial Commit

---
+++ PremiumCard34ML/PremiumCard34MLApp.swift
@@ -0,0 +1,17 @@
+//
+//  PremiumCard34MLApp.swift
+//  PremiumCard34ML
+//
+//  Created by Youssef Emad on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct PremiumCard34MLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PremiumCard34MLUITests/PremiumCard34MLUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PremiumCard34MLUITestsLaunchTests.swift
+//  PremiumCard34MLUITests
+//
+//  Created by Youssef Emad on 15/11/2023.
+//
+
+import XCTest
+
+final class PremiumCard34MLUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
